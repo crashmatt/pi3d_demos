9d5edf5a4cc99fceea8c67cbca30c58f5614bda7
crashmatt <uavflightdirector@gmail.com>
2014-07-27 23:58:29 +0200

    Added HUDConfig.py

---
+++ HUDConfig.py
@@ -0,0 +1,22 @@
+'''
+Created on 23 Jul 2014
+
+@author: matt
+'''
+
+class HUDConfig(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.filename = "HUD.cfg"
+        self.layers = []
+        
+    def get_layers(self):
+        return self.layers
+    
+    
