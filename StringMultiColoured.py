b1f9fe3f91c60e8c008855830989fe665c143d04
crashmatt <uavflightdirector@gmail.com>
2015-10-18 14:02:59 +0200

    More and smoother

--- StringMultiColoured.py
+++ StringMultiColoured.py
@@ -40,6 +40,7 @@ class junk(object):
         self.valA = 0.0
         self.fps = 0.0
         self.strA = "A moving string"
+        self.angle = 0.0
         
 
 KEYBOARD = pi3d.Keyboard()
@@ -57,6 +58,7 @@ font_colour = (255,255,255,255)
 
 the_junk = junk()
 text_pos = HHWIDTH
+text_delta_pos = -3
 
 working_directory = os.path.dirname(os.path.realpath(__file__))
 font_path = os.path.abspath(os.path.join(working_directory, 'fonts', 'FreeSans.ttf'))
@@ -82,6 +84,7 @@ text.add_text_block(newtxt)
 
 
 textSize = 0.1
+textGrowth=0.01
 sizingText = TextBlock(-100, 150, 0.1, 0.0, 15, None, None, text_format="Resizing text", size=textSize, spacing="F", space=0.05, colour=(1.0, 1.0, 0.0, 1.0) )
 text.add_text_block(sizingText)
 
@@ -98,6 +101,7 @@ text.add_text_block(spacingText)
 
 #String colour and alpha
 textAlpha = 0.1
+alphaGrowth = 0.01
 alphaText = TextBlock(-250, -300, 0.0, 90.0, 15, None, None, text_format="Alpha change", size=0.99, spacing="C", space=0.6, colour=(0.99, 0.99, 0.99, textAlpha) )
 text.add_text_block(alphaText)
 
@@ -113,36 +117,56 @@ alphaGradient = TextBlockColourGradient((1.0,1.0,1.0,1.0),(1.0,1.0,1.0,0.25))
 alphaGradientText = TextBlock(50, -100, 0.1, 0.0, 16, None, None, text_format="Alpha Gradient", size=0.7, spacing="C", space=0.6, colour=alphaGradient )
 text.add_text_block(alphaGradientText)
 
+
+everythingColour = TextBlockColourGradient((1-textAlpha,0.0,textAlpha,1.0),(textAlpha,0.0,1-textAlpha,1.0))
+everythingText = TextBlock(100, 200, 0.1, textRotation, 15, the_junk, "angle", text_format="Angle: {:3.0f}", size=0.7, spacing="F", space=0.05, colour=everythingColour, justify=0.5 )
+#everythingText = TextBlock(100, 200, 0.1, textRotation, 15, the_junk, "angle", text_format="Angle: {:2.3f}", size=0.7, spacing="C", space=0.6, colour=(1.0, 1.0, 1.0, 0.5), justify=0.5 )
+text.add_text_block(everythingText)
+
+
 frame_count = 0
 end_time = time.time() + 1.0
 
 while DISPLAY.loop_running():
-    text_pos -= 3
+    text_pos += text_delta_pos
     if text_pos < -HHWIDTH:
-        text_pos = HHWIDTH
+        text_delta_pos = 3
+    elif text_pos > HHWIDTH:
+        text_delta_pos = -3
     moving_text.set_position(x=text_pos)
+    
 #    moving_text.last_value = None        # a hack to trigger a redraw in a new position
     the_junk.valA += 0.01
     the_junk.valA *= 1.0123
     if the_junk.valA > 1000.0:
         the_junk.valA *= 0.01
         
-    textSize += 0.01
-    if textSize > 0.99:
+    textSize += textGrowth
+    if textSize > 1.0:
+        textSize = 1.0
+        textGrowth = -0.05
+    elif textSize < 0.1:
         textSize = 0.1
- 
+        textGrowth = +0.01
+        
     sizingText.set_text(size=textSize)
     
     textRotation += 1.0
     if textRotation >= 180.0:
         textRotation -= 360.0
         
+    the_junk.angle = textRotation
+        
     rotatingText.set_position(rot=textRotation)
     rotatingChars.set_text(char_rot=textRotation)
     
-    textAlpha += 0.01
+    textAlpha += alphaGrowth
     if textAlpha >= 1.0:
-        textAlpha = 0.1
+        textAlpha = 1.0
+        alphaGrowth = -0.01
+    elif textAlpha < 0.0:
+        textAlpha = 0.0
+        alphaGrowth = 0.02
     alphaText.colouring.set_colour(alpha=textAlpha)
     
     colour_angle = math.radians(textRotation)
@@ -153,6 +177,11 @@ while DISPLAY.loop_running():
     colourText.colouring.set_colour(colour)
     
     spacingText.set_text(space=textSize)
+    
+#    everythingColour.set_colour( (0.1, 0.0 , 1.0, 1.0), (1.0 , 0.0, 0.1, 1.0) )
+    everythingText.set_text(space=textSize*0.25, set_pos=False, set_colour=False)
+    everythingText.set_position(x=text_pos-50, y=150+(text_pos*0.2), rot=textRotation)
+    everythingColour.set_colour( (1.0, textAlpha , 0.0, 1.0), (0.0, textAlpha, 1.0, 1.0) )
     
     now = time.time()
     frame_count += 1
